8fe339e96fea410a988dfba82fd0bb29dbd20468
Dylan Paucar <62946834+dylanp14132@users.noreply.github.com>
2026-09-02 14:31:23 -0400

    Add "--nc" flag functionality

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include "parser.h"
 
 #define START_ADDR 0x47A0
 
 int main(const int argc, char** argv) {
-    if (argc != 2) {
+    if (argc < 2) {
         fprintf(stderr, "invalid number of arguments\n");
         return 1;
+    }
+
+    uint8_t flag_bits = 0;
+
+    for (size_t i = 2; i < argc; ++i) {
+        if (strcasecmp(argv[i], "--nc") == 0) {flag_bits = flag_bits | 0x01;}
+        else {
+            fprintf(stderr, "unknown flag \"%s\"\n", argv[i]);
+            return 1;
+        }
     }
 
     FILE* in = fopen(argv[1],"r");
@@ -28,7 +39,7 @@ int main(const int argc, char** argv) {
                 "DATA_RADIX = HEX;		% Data Format %\n\n"
                 "CONTENT\n"
                 "BEGIN\n\n\0", out);
-    fprintf(out, "[0000..%X]    :    0;\n\n", START_ADDR - 1);
+    fprintf(out, "[0000..%X]    :    00;\n\n", START_ADDR - 1);
 
     uint16_t curr_addr = START_ADDR;
     char buf[256] = {0};
@@ -42,15 +53,17 @@ int main(const int argc, char** argv) {
         }
         if (is_blank) {continue;}
         format_line(buf);
-        fprintf(out, "%X            :	 0%X;  --%s\n", curr_addr, instr.word >> 4, buf);
+        if (flag_bits & 0x01) {fprintf(out, "%X            :	 0%X;\n", curr_addr, instr.word >> 4);}
+        else {fprintf(out, "%X            :	 0%X;  --%s\n", curr_addr, instr.word >> 4, buf);}
         ++curr_addr;
         if (!instr.has_op) {continue;}
         fprintf(out, "%X            :	 0%X;\n", curr_addr, instr.word & 0x0F);
         ++curr_addr;
     }
 
-    fprintf(out, "\n[%X..7FFF]    :	 0;\n\n", curr_addr);
+    fprintf(out, "\n[%X..7FFF]    :	 00;\n\n", curr_addr);
     fputs("END ;\0", out);
+    fclose(in);
     fclose(out);
     printf("Success!\n");
 
